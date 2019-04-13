70c186af62586d9e5eb3cb7c05cd9caf52b378e3
CromixPT <tiago.g.candeias@gmail.com>
2019-04-13 19:39:42 +0100

    Estrutura inicial de preparação para testes de eventos/delegates

---
+++ Assets/Sala.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="MVC Perdido/Sala")]
+public class Sala : ScriptableObject
+{
+    public string nome;
+    [TextArea(1, 30)]
+    public string descricao;
+
+    
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        OnGameStart();
+    }
+
+    private void OnGameStart()
+    {
+        throw new NotImplementedException();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
