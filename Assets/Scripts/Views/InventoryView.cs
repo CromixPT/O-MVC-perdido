ee6838ce9596e999248f9acc04facb5964dc2628
danicas <daniel_pina18@hotmail.com>
2019-04-22 10:37:41 +0100

    Criação de InventoryView.cs

---
+++ Assets/Scripts/Views/InventoryView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryView : MonoBehaviour
+{
+    private Text inventoryText;
+    private void Awake()
+    {
+        inventoryText = GetComponent<Text>();
+        //Player.OnPlayerInfoChange += updateView;
+    }
+
+    private void updateView(object source, EventArgs args)
+    {
+        //cast de object para player para conseguir ler os atributos.
+        //Player player = (Player)source;
+        //playerText.text = "Vida: \t" + player.life.ToString() + "\n" + "Ataque: \t" + player.attackPower.ToString();
+    }
+}
