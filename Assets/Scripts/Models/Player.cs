18f19050a2ef0ff8e8c04a585f499cf448a52ea0
ViCor44 <victor.a.correia@gmail.com>
2019-04-24 22:17:49 +0100

    Acrescentados métodos a Player

--- Assets/Scripts/Models/Player.cs
+++ Assets/Scripts/Models/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     public static event ViewPlayerUpdateEventHandler OnPlayerInfoChange;
     public static event ViewPlayerUpdateEventHandler OnPlayerDead;
 
+    // inicializa as variaveis e lança event
     public void PlayerStart()
     {
         life = 100;
@@ -17,6 +18,7 @@ public class Player : MonoBehaviour
         PlayerInfoUpdate();
     }
 
+    // altera a variavel life e lança event 
     public void PlayerLifeChange(int quantity)
     {
         life += quantity;
@@ -33,6 +35,7 @@ public class Player : MonoBehaviour
         PlayerInfoUpdate();
     }
 
+    // altera a variavel attackPower e lança event
     public void PlayerAttackChange(int quantity)
     {
         attackPower += quantity;
@@ -46,6 +49,16 @@ public class Player : MonoBehaviour
             life = 12;
         }
         PlayerInfoUpdate();
+    }
+
+    public int PlayerLife()
+    {
+        return life;
+    }
+
+    public int PlayerPower()
+    {
+        return attackPower;
     }
 
     public void PlayerInfoUpdate()
