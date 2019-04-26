021535257e63048aa23f9b9a8b6f8cca81b2aa1c
ViCor44 <victor.a.correia@gmail.com>
2019-04-26 14:23:49 +0100

    Alteração aos atributos de items

--- Assets/Scripts/Models/Item.cs
+++ Assets/Scripts/Models/Item.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item
-{
-    public int id;
+{    
     public string title;
     public string description;
-
     public Dictionary<string, int> stats = new Dictionary<string, int>();
 
-    public Item(int id, string title, string description, Dictionary<string, int> stats)
+    public Item(string title, string description, Dictionary<string, int> stats)
     {
-        this.id = id;
         this.title = title;
         this.description = description;        
         this.stats = stats;
@@ -20,7 +17,6 @@ public class Item
 
     public Item(Item item)
     {
-        this.id = item.id;
         this.title = item.title;
         this.description = item.description;        
         this.stats = item.stats;
